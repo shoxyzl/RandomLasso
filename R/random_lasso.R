05552852e4d60693fe5d741d817e37a6fcda68a2
Branden Ciranni <branden.ciranni@gmail.com>
2022-04-25 18:45:13 -0400

    code cleanup

--- R/random_lasso.R
+++ R/random_lasso.R
@@ -1,10 +1,26 @@
 library(parallel)
 library(glmnet)
-# sample size n
 
 
+################################################################################
+############################# Utility Functions ################################
+################################################################################
 
-# generic regularized regression function
+
+bootstrap <- function(X, y) {
+  n <- dim(X)[1]
+  bootstrap_ind <- sample(1:n, n, replace=TRUE)
+  X.b <- X[bootstrap_ind,]
+  y.b <- y[bootstrap_ind]
+  return(list(X = X.b, y=y.b))
+}
+
+
+################################################################################
+########################### Generic Regularized GLM ############################
+################################################################################
+
+
 regularized_glm <- function(X, y, alpha=0, continuous=T, exclude=NULL) {
   X <- as.matrix(X)
   if (continuous) {
@@ -14,10 +30,15 @@ regularized_glm <- function(X, y, alpha=0, continuous=T, exclude=NULL) {
   else {
     y <- as.factor(y)
     cv <- cv.glmnet(X, y, alpha=alpha, exclude=exclude, family='binomial')
-    model <- glmnet(X, y, alpha=alpha, exclude=exclude, family='binomial', lambda=cv$lambda.min)
+    model <- glmnet(X, y, alpha=alpha, exclude=exclude, family='binomial', 
+                    lambda=cv$lambda.min)
   }
   return (model)
 }
+
+################################################################################
+############################### Lasso & Ridge ##################################
+################################################################################
 
 
 lasso <- function(X, y, continuous=T, exclude=NULL) {
@@ -28,51 +49,50 @@ ridge <- function(X, y, continuous=T, exclude=NULL) {
   return(regularized_glm(X, y, 0, continuous, exclude))
 }
 
+################################################################################
+############################### Generate Step ##################################
+################################################################################
+
 random_lasso.generate_i <- function(X, y, q1, continuous) {
+  bs <- bootstrap(X,y)
   
-  n <- dim(X)[1]
   q <- dim(X)[2]
-  
-  bootstrap_ind <- sample(1:n, n, replace=TRUE)
-  X.b <- X[bootstrap_ind,]
-  y.b <- y[bootstrap_ind]
-  
   exclude <- sample(1:q, q-q1, replace=FALSE)
   
-  beta_j_hat <- coef(lasso(X.b, y.b, continuous, exclude))
+  beta_j_hat <- coef(lasso(bs$X, bs$y, continuous, exclude))
   return(as.vector(beta_j_hat[2:(q+1),]))
 }
 
 
 random_lasso.generate <- function(X, y, B, q1, continuous) {
-  n <- dim(X)[1]
   q <- dim(X)[2]
+  
   beta_hat <- matrix(NA, nrow=B, ncol=q)
   for (i in 1:B) {
     beta_hat[i,] <- t(as.matrix(random_lasso.generate_i(X, y, q1, continuous)))
   }
+  
   importance <- apply(t(beta_hat), 1, function(x) abs(1/B*sum(x)))
   return(importance)
 }
 
+################################################################################
+############################### Select Step ####################################
+################################################################################
+
+
 random_lasso.select_i <- function(X, y, B, q2, importance, continuous) {
-  n <- dim(X)[1]
+  bs <- bootstrap(X,y)
+  
   q <- dim(X)[2]
-  
-  bootstrap_ind <- sample(1:n, n, replace=TRUE)
-  X.b <- X[bootstrap_ind,]
-  y.b <- y[bootstrap_ind]
-  
   include <- sample(1:q, q2, replace=FALSE, prob = importance)
   exclude <- (1:q)[-include]
   
-  beta_j_hat <- coef(lasso(X.b, y.b, continuous, exclude))
+  beta_j_hat <- coef(lasso(bs$X, bs$y, continuous, exclude))
   return(as.vector(beta_j_hat[2:(q+1),]))
 }
 
 random_lasso.select <- function(X, y, B, q2, importance, continuous) {
-  
-  n <- dim(X)[1]
   q <- dim(X)[2]
   
   beta_hat <- matrix(NA, nrow=B, ncol=q)
@@ -83,6 +103,11 @@ random_lasso.select <- function(X, y, B, q2, importance, continuous) {
   beta_j_final <- apply(t(beta_hat), 1, function(x) 1/B * sum(x))
   return(beta_j_final)
 }
+
+################################################################################
+############################### Random Lasso ###################################
+################################################################################
+
 
 random_lasso <- function(X, y, B, q1, q2, continuous) {
   importance <- random_lasso.generate(X, y, B, q1, continuous)
